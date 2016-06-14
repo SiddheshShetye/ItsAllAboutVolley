01db7ba1f04fcc815c86336ac822a6687db45b25
Siddhesh Shetye <siddhesh.pro@gmail.com>
2016-06-14 09:34:58 +0530

    added authorization headers logic

--- app/src/main/java/aboutvolley/sidroid/com/itsallaboutvolley/request/EntityMapperRequestImpl.java
+++ app/src/main/java/aboutvolley/sidroid/com/itsallaboutvolley/request/EntityMapperRequestImpl.java
@@ -20,16 +20,16 @@ public class EntityMapperRequestImpl extends EntityMapperRequest {
 
     private JSONObject mRequestBody;
     private boolean needAuthorisationHeader;
-    private String mValue1;
-    private String mValue2;
     private String mTag;
     private int mMethod;
+    private Map<String,String> mAuthHeader;
 
     public EntityMapperRequestImpl(int method, String url, JSONObject requestBody, Response.ErrorListener listener, Response.Listener responseListener, String tag) {
         super(method, url, listener, responseListener);
         mRequestBody = requestBody;
         mTag = tag;
         mMethod = method;
+        mAuthHeader = new HashMap<>();
     }
 
 
@@ -45,33 +45,30 @@ public class EntityMapperRequestImpl extends EntityMapperRequest {
         mMethod = method;
     }
 
-    public void setAuthHeaderCredentials(String value1,String value2){
-        if (TextUtils.isEmpty(value1) || TextUtils.isEmpty(value2)) {
+    public void setAuthHeaderCredentials(Map<String,String> authHeaders){
+        if (authHeaders == null) {
             needAuthorisationHeader = false;
             return;
         }
-        this.mValue1 = value1;
-        this.mValue2 = value2;
+        mAuthHeader = authHeaders;
         needAuthorisationHeader = true;
     }
 
     @Override
     public Map<String, String> getCustomHeaders() {
         HashMap<String,String> headers;
-        /**
-         * TODO:
-         * uncomment below section after writing logic for getAuthorizationHeadersMap
-         * so you can provide your own authentication headers
-         */
-//        if (needAuthorisationHeader){
-//            headers = getAuthorizationHeadersMap(mValue1, mValue2);
-//            needAuthorisationHeader = false;
-//        }else
+
+        if (needAuthorisationHeader){
+            headers = (HashMap<String, String>) mAuthHeader;
+            needAuthorisationHeader = false;
+        }else
             headers = (HashMap<String, String>) super.getCustomHeaders();
         if (mMethod == Method.GET)
             headers.put("Content-Type", getBodyContentType());
         return headers;
     }
+
+
 
     @Override
     public Class<?> getMapperClass() {
